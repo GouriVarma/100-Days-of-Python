d0d9174f89580223bc3406c94a6dee195cb5428e
Gouri G Varma <96877313+GouriVarma@users.noreply.github.com>
2023-03-25 19:36:39 +0530

    Delete beginner directory

--- beginner/bandname_generator.py
+++
@@ -1,9 +0,0 @@
-#DAY 1 PROJECT - BAND NAME GENERATOR
-
-
-print('Welcome to Band Generator Game')
-city = input("What's the city you were born? ")
-pet = input("What's your pet's name? ")
-print("Your band name could be ",city+' '+pet)
-
-
